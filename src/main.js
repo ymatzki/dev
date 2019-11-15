2fccce676f77ed157a289d96b03343f712060813
Yusaku Matsuki <ymatzki@gmail.com>
2019-11-16 01:07:10 +0900

    Tweak

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import router from './router'
 
-Vue.use(BootstrapVue)
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
